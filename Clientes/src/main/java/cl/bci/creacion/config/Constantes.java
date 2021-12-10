952c8f6bdb4abb88f8d85a9318060d0c7219471f
DanielaFloresC <dflores874@gmail.com>
2021-12-10 12:48:59 -0300

    java test
    commit inicial java test BCI

---
+++ Clientes/src/main/java/cl/bci/creacion/config/Constantes.java
@@ -0,0 +1,22 @@
+package cl.bci.creacion.config;
+
+public class Constantes {
+
+	Constantes() {
+		super();
+	}
+	
+	public static final String REG_EXP_PASS = "^([A-Z]){1}([a-z]+){1,}([0-9]{2})$";
+	public static final String REG_EXP_EMAIL = "^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	public static final String PASS_NO_VALIDA = "Password no valido";
+	public static final String EMAIL_NO_VALIDO = "Email no valido";
+	public static final String EMAIL_REGISTRADO = "El correo ya esta Registrado";
+	public static final String PARAM_REQUERIDO = "Parametros requeridos";
+	public static final String PARAM_REQUERIDO_PHONES = "Telefono requerido";
+	public static final String PARAM_REQUERIDO_EMAIL = "Correo requerido";
+	public static final String PARAM_REQUERIDO_NOMBRE = "Nombre requerido";
+	public static final String PARAM_REQUERIDO_PASSWORD = "Password requerido";
+	public static final String ERROR_CREACION = "No se pudo crear";
+	public static final String IS_ACTIVE = "activo";
+	public static final String SUCCESS = "success";
+}
