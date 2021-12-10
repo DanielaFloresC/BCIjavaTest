d339f791cc78196d096be9c67643f0080174cf4b
DanielaFloresC <dflores874@gmail.com>
2021-12-10 12:45:00 -0300

    firstCommit
    test java BCI

---
+++ testClienteBCI/src/main/java/cl/claro/netConfiguration/TestClienteBciApplication.java
@@ -0,0 +1,13 @@
+package cl.claro.netConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClienteBciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestClienteBciApplication.class, args);
+	}
+
+}
---
+++ testClienteBCI/src/test/java/cl/claro/netConfiguration/TestClienteBciApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.claro.netConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestClienteBciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
